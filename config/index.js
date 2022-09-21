5ce7cfe3628c58ef8fbcb26b635caa9479dadd2c
Lionel <elrumordelaluz@hotmail.com>
2022-09-21 16:22:02 +0200

    Add assets

--- config/index.js
+++ config/index.js
@@ -1,3 +1,8 @@
 export const website = {
   title: 'Come si vota',
+  shortDesc: 'Istruzioni semplici per il voto del 25 settembre',
+  description:
+    'Istruzioni semplici per le prossime elezioni del 25 settembre 2022, in cui tutti i cittadini italiani sono chiamati a votare per rinnovare il Parlamento.',
+  url: 'https://www.comesivota.eu',
+  image: '/logo.png',
 }
